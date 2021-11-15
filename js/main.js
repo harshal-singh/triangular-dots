834d0e362eb3e8a97c87a01904ff67fe306cd662
harshal-singh <dharma3136@gmail.com>
2021-11-15 13:42:40 +0530

    responsive added

--- js/main.js
+++ js/main.js
@@ -16,11 +16,14 @@ $(window).scroll(function () {
     }
 });
 
-if (location.pathname !== "/triangular-dots/") {
+if (
+    location.pathname !== "/triangular-dots/" ||
+    location.pathname !== "/triangular-dots/single-blog.html" ||
+    location.pathname !== "/triangular-dots/single-case-study.html" ||
+    location.pathname !== "/triangular-dots/single-service.html"
+) {
     document.querySelector(
         "body"
     ).innerHTML = `<section class="bg-light d-flex align-items-center justify-content-center" style="width: 100vw; height: 100vh"><div class="container text-center"><h2>⚠</h2><h4 class="font-b mb-5 mt-4">You need permission <br /> to view this page!
     </h4> <a href="/triangular-dots/" class="btn btn-outline-dark">Go to Home Page</a> </div></section>`;
-} else {
-    console.log("home page");
 }
