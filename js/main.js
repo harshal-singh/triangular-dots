04070bd03083bc1e7883879ffe60f9669bee3a72
harshal-singh <dharma3136@gmail.com>
2021-11-15 13:30:02 +0530

    responsive added

--- js/main.js
+++ js/main.js
@@ -16,11 +16,7 @@ $(window).scroll(function () {
     }
 });
 
-if (
-    location.pathname !== "/" ||
-    location.pathname !== "/triangular-dots/index.html" ||
-    location.pathname !== "/triangular-dots/"
-) {
+if (location.pathname !== "/triangular-dots/index.html" || location.pathname !== "/triangular-dots/") {
     document.querySelector(
         "body"
     ).innerHTML = `<section class="bg-light d-flex align-items-center justify-content-center" style="width: 100vw; height: 100vh"><div class="container text-center"><h2>⚠</h2><h4 class="font-b mb-5 mt-4">You need permission <br /> to view this page!
