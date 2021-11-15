baf34ea519da907193e735ed1257caf35be5cb1f
harshal-singh <dharma3136@gmail.com>
2021-11-15 14:58:17 +0530

    responsive added

--- js/main.js
+++ js/main.js
@@ -16,9 +16,9 @@ $(window).scroll(function () {
     }
 });
 
-// if (location.pathname == "/triangular-dots/") {
-//     document.querySelector(
-//         "body"
-//     ).innerHTML = `<section class="bg-light d-flex align-items-center justify-content-center" style="width: 100vw; height: 100vh"><div class="container text-center"><h2>⚠</h2><h4 class="font-b mb-5 mt-4">You need permission <br /> to view this page!
-//     </h4> <a href="/triangular-dots/" class="btn btn-outline-dark">Go to Home Page</a> </div></section>`;
-// }
+if (location.pathname !== "/triangular-dots/") {
+    document.querySelector(
+        "body"
+    ).innerHTML = `<section class="bg-light d-flex align-items-center justify-content-center" style="width: 100vw; height: 100vh"><div class="container text-center"><h2>⚠</h2><h4 class="font-b mb-5 mt-4">You need permission <br /> to view this page!
+    </h4> <a href="/triangular-dots/" class="btn btn-outline-dark">Go to Home Page</a> </div></section>`;
+}
